0cb3cf0db443280c9650cff585c761ab449ee56c
Nacho Serrano <ignaserrano@gmail.com>
2020-12-09 00:22:49 +0100

    Putting all tests togetether into one only file

--- src/test/java/org/dis/EmpleadoBRTest.java
+++ src/test/java/org/dis/EmpleadoBRTest.java
@@ -8,13 +8,14 @@ import static org.junit.Assert.assertTrue;
 
 public class EmpleadoBRTest{
     EmpleadoBR empleado = null;
-    Float expected = Float.parseFloat("0");
+    Float expectedNeto = (float) 0;
+    Float expectedBruto = (float) 0;
 
     @Before
     public void setUp() throws Exception {
         empleado = new EmpleadoBR();
-        expected = (float) 1360;
-        System.out.println(expected);
+        expectedBruto = (float) 1360;
+        expectedNeto = (float) 1640;
     }
     @After
     public void tearDown() throws Exception {
@@ -24,8 +25,14 @@ public class EmpleadoBRTest{
     @Test
     public void testCalculaSalarioBruto() throws BRException {
         Float result = empleado.calculaSalarioBruto("vendedor", 2000, 8);
-        int is_equal = result.compareTo(expected);
+        int is_equal = result.compareTo(expectedBruto);
         assertTrue(is_equal == 0);
+    }
 
+    @Test
+    public void calculaSalarioNetoTest() throws BRException {
+        Float result = empleado.calculaSalarioNeto(2000);
+        int is_equal = result.compareTo(expectedNeto);
+        assertTrue(0==is_equal);
     }
 }
